4ea1d181644d9ec07ca4ddbe75016ac2948a2b44
aziznal <aziznal.dev@gmail.com>
2025-02-17 17:16:26 +0300

    add new lines between darkmode functions

--- lib/common/darkmode/client.ts
+++ lib/common/darkmode/client.ts
@@ -4,15 +4,18 @@ import { DARKMODE_COOKIE_NAME } from "./core";
 export function getIsDarkmode_client() {
   return Cookies.get(DARKMODE_COOKIE_NAME) === "true";
 }
+
 export function setDarkmode_client(value: "true" | "false") {
   Cookies.set(DARKMODE_COOKIE_NAME, value);
 
   updateHtmlTagClass();
 }
+
 export function toggleDarkmode_client() {
   if (getIsDarkmode_client()) setDarkmode_client("false");
   else setDarkmode_client("true");
 }
+
 function updateHtmlTagClass() {
   const isDarkmode = getIsDarkmode_client();
 
--- lib/common/darkmode/server.ts
+++ lib/common/darkmode/server.ts
@@ -4,9 +4,11 @@ import { DARKMODE_COOKIE_NAME } from "./core";
 export async function getIsDarkmode_server() {
   return (await cookies()).get(DARKMODE_COOKIE_NAME)?.value === "true";
 }
+
 export async function setDarkmode_server(value: "true" | "false") {
   return (await cookies()).set(DARKMODE_COOKIE_NAME, value);
 }
+
 export async function toggleDarkmode_server() {
   if (await getIsDarkmode_server()) await setDarkmode_server("false");
   else await setDarkmode_server("true");
